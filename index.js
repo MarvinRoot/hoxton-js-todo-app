5c8cf9c790f754d5eeec8b7a683153d48d49c56d
marvin <aliajmarvin@gmail.com>
2021-11-27 11:51:04 +0100

    Finished the Incompleted Todos render.

--- index.js
+++ index.js
@@ -102,8 +102,52 @@ function renderCompletedTodos() {
 }
 
 
+function renderIncompletedTodos(){
+    const incompletedTodos = getIncompletedTodos()
+    todoList.innerHTML = ''
+
+    for(const todo of incompletedTodos){
+        const liEl = document.createElement('li')
+        liEl.setAttribute('class', 'todo')
+
+        const todoSection = document.createElement('div')
+        todoSection.setAttribute('class', 'todo-section')
+            const completedCheckbox = document.createElement('input')
+            completedCheckbox.setAttribute('type', 'checkbox')
+            completedCheckbox.setAttribute('class', 'completed-checkbox')
+        todoSection.append(completedCheckbox)
+
+        const textSection = document.createElement('div')
+        textSection.setAttribute('class', 'text-section')
+            const todoText = document.createElement('p')
+            todoText.setAttribute('class', 'text-section')
+            todoText.textContent = todo.title
+        textSection.append(todoText)
+
+        const buttonSection = document.createElement('div')
+        buttonSection.setAttribute('class', 'button-section')
+            const editButton = document.createElement('button')
+            editButton.setAttribute('class', 'edit')
+            editButton.textContent = 'Edit'
+            const deleteButton = document.createElement('button')
+            deleteButton.setAttribute('class', 'delete')
+            deleteButton.textContent = 'Delete'
+        buttonSection.append(editButton, deleteButton)
+
+        completedCheckbox.checked = todo.completed
+        completedCheckbox.addEventListener('click', function(){
+            toggleTodo(todo)
+            render()
+        })
+
+        liEl.append(todoSection, textSection, buttonSection)
+        todoList.append(liEl)
+    }
+}
+
 function render() {
     renderCompletedTodos()
+    renderIncompletedTodos()
 }
 
 render()
